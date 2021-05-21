057652a9ce43d3a28ce29a24160d8e3fa2ead6ff
kenmori <kenjimoritata@gmail.com>
2021-05-21 18:10:21 +0900

    feat: add jssStyle for remove

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,4 +1,4 @@
-import App from "next/app"
+import { useEffect } from "react"
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
@@ -7,6 +7,13 @@ import { themeDark, themeLight } from "lib/theme"
 
 
 export default function MyApp({ Component, pageProps }){
+  useEffect(() => {
+    // remove the server-sizde injectedd CSS
+    const jssStyle = document.querySelector("jss-server-side");
+    if(jssStyle && jssStyle.parentNode){
+      jssStyle.parentNode.removeChild(jssStyle)
+    }
+  }, [])
   return (
     <ThemeProvider theme={ false ? themeDark : themeLight}>
       <CssBaseline />
