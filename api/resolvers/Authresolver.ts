d69bd4c028b43598676b6c69f39ec2699df3bdda
kenmori <kenjimoritata@gmail.com>
2021-05-22 15:54:39 +0900

    feat: login mutation

--- api/resolvers/Authresolver.ts
+++ api/resolvers/Authresolver.ts
@@ -34,4 +34,31 @@ export class AuthResolver {
 
     return { user, token };
   }
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("input") { email, password }: AuthInput
+  ): Promise<UserResponse> {
+    // 1. check for an exsting email
+    const existingUser = await UserModel.findOne({ email })
+    if(!existingUser){
+      throw new Error("Invalid login")
+    }
+    // 2. check if the password is valid
+    const valied = await bcrypt.compare(existingUser.password, password)
+
+    if(!valied){
+      throw new Error("password is invalid");
+    }
+
+    // 3. store user id on the token payload
+    const payload = {
+      id: existingUser.id
+    }
+
+    const token = jwt.sign(
+      payload,
+      process.env.SESSON_SCRET || "ffafafafafa"
+    )
+    return { user: existingUser.id, token };
+  }
 }
